c19a5f132142b588caf94364735b40950231f3e4
Eliasari <elia21196@gmail.com>
2022-03-07 15:20:47 +0700

    praktikum2

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div class="container">
+      <div class="card col-sm-4">
+        <div class="card-header bg-success text-white">
+          <h4>Cicilan Bank</h4>
+        </div>
+        <div class="card-body">
+          Nominal
+          <input type="number" id="nominal" class="form-control" />
+          Bunga
+          <input type="number" id="bunga" class="form-control" />
+          <label for="periode">Periode</label>
+
+          <select id="Periode" class="form-control">
+            <option value="1 Bulan">1 Bulan</option>
+            <option value="4 Bulan">4 Bulan</option>
+            <option value="8 bulan">8 Bulan</option>
+            <option value="12 Bulan">12 Bulan</option>
+          </select>
+          <br></br>
+          <button type="button" class="btn btn-success btn-block"
+          onclick="count()">
+            Hitung
+          </button>
+          <br></br>
+          <br></br>
+          Hasil
+          <input type="text" id="hasil" class="form-control" readonly />
+        </div>
+      </div>
     </div>
+
   );
 }
 
